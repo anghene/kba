9e77034ece7c2dda396677f9987e37b2ee2d341d
Vlad Anghene <vlad.anghene@gmail.com>
2015-11-12 01:32:15 +0200

    made a website

---
+++ compass.web/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Compass website - A KBA attempt using Location for Vlad Anghene's final paper </title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+<!-- JUMBOTRON class provided by github.com/andygimma , originally found at http://www.bootply.com/tagged/jumbotron -->
+	<div class="jumbotron ">
+	  <div class="container bs-docs-header">
+	    <h1>Compass v1.alpha</h1>
+	    <p>Hi. This is a first attempt to approach my final thesis paper. Thanks for coming over and showing interest in my work regarding <strong>Knowledge Based Authentication</strong> and Location Services. It is just a starting point to create a beautiful app designed to help users login to their fav services without asking for their password.</p>
+	    <p><a href="https://medium.com/@anghene/newauth-signing-in-in-the-new-millenium-d8930c288e1c" class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
+
+	  </div>
+
+	</div>
+
+	<div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h2>About me</h2>
+          	<div class="caption">
+				Hello. I am Vlad Anghene (<i class="fa fa-twitter"></i> <a href="http://twitter.com/avaloncoder" target="_blank" title="check out my profile, follow me and let's tweet!">@avaloncoder</a>) </br> I am a final year <strong> computer science </strong> student at <a href="http://uab.ro" title="My Uni's website"> Universitatea 1 Decembrie 1918</a> in Alba Iulia.
+      		</div>
+        </div>
+        <div class="col-md-4">
+          <h2>About this project</h2>
+          <p><strong>Compass</strong> is a location based KBA app. You can find all codebase (along with docs and everything else) on <a href="https://github.com/anghene/locationkba" target="_blank" title="Go on github"> Git <i class="fa fa-github"></i></a> . </p>
+       </div>
+        <div class="col-md-4">
+          <h2>Current Status</h2>
+          <p>Compass v1.alpha is still in development. Earliest working beta will be made available in december 2015 (est.)</p>
+	
+        </div>
+        </div>
+<div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center newsletterbox">
+          <div><h4>Keep up to date if you're interested in what I'm developing here.</h4></div>
+          <form action="" method="post">                        
+		        	
+		        	<div class="input-group input-group-lg">
+
+						<span class="input-group-addon" id="sizing-addon1"><i class="fa fa-inbox fa2"></i></span>
+						<input type="text" class="form-control" placeholder="your@email.com" aria-describedby="sizing-addon1">
+					</div>
+                    <br />
+                    <input type="submit" value="Subscribe Now!" class="btn btn-primary btn-lg" />
+           </form>	
+   		</div>	
+</div>
+<footer class="bs-docs-footer">
+  <h6>
+  Made with <span class="glyphicon glyphicon-heart"></span> in Alba Iulia.<br>
+  Using Bootstrap3 and Sublime.
+  </h6>
+</footer>
+</body>
+</html>
