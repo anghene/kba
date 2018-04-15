8348f94fabd833f2ba1a604343dfbab4a590c382
ava <vlad.anghene@gmail.com>
2018-04-15 23:54:53 +0300

    small name edit

--- compass.web/index.php
+++ compass.web/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Welcome to Compass website - A KBA attempt using Location for Vlad Anghene's final paper </title>
+	<title>Welcome to Compass website - A KBA attempt using Location for my final paper </title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="./css/style.css">
